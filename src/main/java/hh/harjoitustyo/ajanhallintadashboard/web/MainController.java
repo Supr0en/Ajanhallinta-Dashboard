e6b3bd4b6c3dde2fb2d29f4af9b65e87488c163b
Supr0en <erik.ruotsalainen1@gmail.com>
2025-10-26 19:59:27 +0200

    Pohjistus harjoitustyölle

--- src/main/java/hh/harjoitustyo/ajanhallintadashboard/web/MainController.java
+++ src/main/java/hh/harjoitustyo/ajanhallintadashboard/web/MainController.java
@@ -8,6 +8,18 @@ public class MainController {
 
     @GetMapping("/index")
     public String index() {
-        return "index";
+        return "index"; // index.html - this will be dashboard page where you navigate to other pages
+    }
+    @GetMapping("/weekview")
+    public String weekview() {
+        return "Weekview"; // Weekview.html
+    }
+    @GetMapping("/todolist")
+    public String todolist() {
+        return "Todolist"; // Todolist.html
+    }
+    @GetMapping("/notes")
+    public String notes() {
+        return "Notes"; // Notes.html
     }
 }
