077c5a97f34ce6f46ab7cf73d35a7d54f9927f39
Erik Ruotsalainen <94750392+Supr0en@users.noreply.github.com>
2025-11-09 21:58:04 +0200

    Deleted unused part of code

--- src/main/java/hh/harjoitustyo/ajanhallintadashboard/domain/notes/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package hh.harjoitustyo.ajanhallintadashboard.domain.notes;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NoteRepository extends CrudRepository<Note, Long> {
-
-}
