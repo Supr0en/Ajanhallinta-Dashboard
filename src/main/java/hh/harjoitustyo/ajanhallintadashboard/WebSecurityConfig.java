b388049216095d40a1b8a852a5c0aa5f05a548dc
Supr0en <erik.ruotsalainen1@gmail.com>
2025-11-09 18:24:59 +0200

    Change to WebSecurityConfig file

--- src/main/java/hh/harjoitustyo/ajanhallintadashboard/WebSecurityConfig.java
+++ src/main/java/hh/harjoitustyo/ajanhallintadashboard/WebSecurityConfig.java
@@ -2,10 +2,11 @@ package hh.harjoitustyo.ajanhallintadashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
+@Configuration
 @Controller
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
