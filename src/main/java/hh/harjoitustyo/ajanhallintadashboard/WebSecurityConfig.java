221b644b0c401fb93e1dbdf1786284abe0ecdfa2
Supr0en <erik.ruotsalainen1@gmail.com>
2025-11-09 18:39:16 +0200

    More changes to WebSecurity

--- src/main/java/hh/harjoitustyo/ajanhallintadashboard/WebSecurityConfig.java
+++ src/main/java/hh/harjoitustyo/ajanhallintadashboard/WebSecurityConfig.java
@@ -10,13 +10,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Controller;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
-@Controller
 @EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
     private final UserDetailsService UserDetailsService;
